9e738b477e98ffbbdf4e7f101587b7104548bc0e
Erik 'Dromedda' Gustafsson <erikgustafsson2001@gmail.com>
2023-01-11 08:56:29 +0100

    Made a camera, uses raylibs built in camera, and i just made some vike bindings basically

--- src/scene_main.js
+++ src/scene_main.js
@@ -5,35 +5,25 @@ const Player = require("./player.js");
 const Sensor = require("./sensor.js");
 const Block = require('./block.js')
 
-const SceneMain = {}; 
-
-let camera_pos = v.vec2(0, 0); 
-let camera_offset = v.vec2(0, 0); 
-const camera = v.camera_init(camera_offset, camera_pos, 0, 1); 
-
-SceneMain.init = function() {
+function init() {
 	player = new Player(100, 100);
 	sensor = new Sensor(200, 200, 32, 32);
 	block = new Block(500, 200);
 }
 
-SceneMain.update = function() {
-	v.camera_goto(camera, v.vec2(player.x, player.y)); 
+function update() {
 	player.update();
 	block.update();
 	sensor.update();
 }
 
-SceneMain.draw = function() {
-	v.camera_set(camera); 
-	v.camera_centered(camera); 
+function draw() {
  	r.ClearBackground(r.RAYWHITE);
+ 	r.DrawFPS(4, 4);
 	
 	block.draw();
 	player.draw();
 	sensor.draw();
-	v.camera_unset(); 
- 	r.DrawFPS(4, 4);
 }
 
-module.exports = SceneMain; 
+module.exports = {init, update, draw};
--- vike.js
+++ vike.js
@@ -3,12 +3,6 @@ const r = require('raylib');
 const Vike = {};
 let objects = []; 
 
-Vike.init_window = function(win) {
-	console.log("VIKE::Initialized Window.." + win.title); 
-	r.InitWindow(win.width, win.height, win.title);; 
-	r.SetTargetFPS(win.targetFPS); 
-} 
-
 Vike.add_obj = function(_class, _name) {
 	_class.name = _name; 
 	_class.id = generate_unique_id(); 
@@ -16,40 +10,32 @@ Vike.add_obj = function(_class, _name) {
 	console.log("VIKE::Object Available::" + _name);  
 }
 
+Vike.init_window = function(win) {
+	console.log("VIKE::Initialized Window.." + win.title); 
+	r.InitWindow(win.width, win.height, win.title);; 
+	r.SetTargetFPS(win.targetFPS); 
+} 
+
 Vike.vec2 = function(v1, v2) {
 	return{x:v1, y:v2}; 
 }
 
+// Clamps the pos vec2 to the screen.
+// ent requires x,y,width,height props.
+Vike.clamp_obj_to_screen = function(ent, pos, spd) {
+    let targetX = pos.x * spd; 
+    let targetY = pos.y * spd; 
+    
+    // Clamp player to the screen.
+    if (ent.x + targetX > (r.GetScreenWidth() - ent.width) || ent.x + targetX < 0) { pos.x = 0; }
+    if (ent.y + targetY > (r.GetScreenHeight() - ent.height) || ent.y + targetY < 0) { pos.y = 0; }
+	
+	return pos; 
+}
+
+// just a wrapper for console.log, because who can be bothered to type console.log everytime.
 Vike.log = function(str) {
 	console.log("VIKE::LOG::" + str); 
-}
-
-// TODOO: Might just wanna make the camera var local to vike, making it easier
-// however will limit the amount of cameras to 1.
-// offset and target are vec2's! 
-Vike.camera_init = function(offset, target, rotation, scale) {
-	let camera = r.Camera2D(offset, target, rotation, scale); 
-	return camera; 
-}
-
-Vike.camera_set = function(camera) {
-	// TODO: validate Camera
-	r.BeginMode2D(camera); 
-}
-
-Vike.camera_unset = function() {
-	r.EndMode2D(); 
-}
-
-// TODO: implement smoothing. 
-Vike.camera_goto = function(camera, pos) {
-	// TODO: validate Camera
-	camera.target = pos; 
-}
-
-Vike.camera_centered = function(camera) {
-	// TODO: validate Camera
-	camera.offset = this.vec2(r.GetScreenWidth()/2, r.GetScreenHeight()/2); 
 }
 
 // TODO: this only returns the first result
@@ -66,7 +52,7 @@ Vike.object_get = function(name, id = -1) {
 	return ret; 
 }
 
-// TODOO: Check if offsets work
+// TODO: add validation
 Vike.check_collision2d = function(a, b, aox = 0, aoy = 0, box = 0, boy = 0) {
 	return (
 		a.x + aox < b.x + box + b.width &&
@@ -74,19 +60,6 @@ Vike.check_collision2d = function(a, b, aox = 0, aoy = 0, box = 0, boy = 0) {
 		b.y + boy < a.y + aoy + a.width &&
 		b.y + boy + b.width > a.y + aoy
 	); 
-}
-
-// Clamps the pos vec2 to the screen.
-// ent requires x,y,width,height props.
-Vike.clamp_obj_to_screen = function(ent, pos, spd) {
-    let targetX = pos.x * spd; 
-    let targetY = pos.y * spd; 
-    
-    // Clamp player to the screen.
-    if (ent.x + targetX > (r.GetScreenWidth() - ent.width) || ent.x + targetX < 0) { pos.x = 0; }
-    if (ent.y + targetY > (r.GetScreenHeight() - ent.height) || ent.y + targetY < 0) { pos.y = 0; }
-	
-	return pos; 
 }
 
 function generate_unique_id() {
