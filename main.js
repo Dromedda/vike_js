f5cf329f6d18e56374593f735c6848d59f0cf321
Erik 'Dromedda' Gustafsson <erikgustafsson2001@gmail.com>
2023-01-11 09:04:09 +0100

    This reverts commit 9e738b477e98ffbbdf4e7f101587b7104548bc0e.

--- main.js
+++ main.js
@@ -32,5 +32,4 @@ function main() {
 	}
 	r.CloseWindow();
 }
-
 main(); 
--- src/scene_main.js
+++ src/scene_main.js
@@ -5,25 +5,35 @@ const Player = require("./player.js");
 const Sensor = require("./sensor.js");
 const Block = require('./block.js')
 
-function init() {
+const SceneMain = {}; 
+
+let camera_pos = v.vec2(0, 0); 
+let camera_offset = v.vec2(0, 0); 
+const camera = v.camera_init(camera_offset, camera_pos, 0, 1); 
+
+SceneMain.init = function() {
 	player = new Player(100, 100);
 	sensor = new Sensor(200, 200, 32, 32);
 	block = new Block(500, 200);
 }
 
-function update() {
+SceneMain.update = function() {
+	v.camera_goto(camera, v.vec2(player.x, player.y)); 
 	player.update();
 	block.update();
 	sensor.update();
 }
 
-function draw() {
+SceneMain.draw = function() {
+	v.camera_set(camera); 
+	v.camera_centered(camera); 
  	r.ClearBackground(r.RAYWHITE);
- 	r.DrawFPS(4, 4);
 	
 	block.draw();
 	player.draw();
 	sensor.draw();
+	v.camera_unset(); 
+ 	r.DrawFPS(4, 4);
 }
 
-module.exports = {init, update, draw};
+module.exports = SceneMain; 
