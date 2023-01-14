96267b971db8fb27958ee388de26552c90c1cd4c
Erik 'Dromedda' Gustafsson <erikgustafsson2001@gmail.com>
2023-01-14 18:20:04 +0100

    added some score.

--- src/block.js
+++ src/block.js
@@ -1,6 +1,8 @@
 const r = require('raylib'); 
 const v = require('../vike.js');  
 
+let score = 0; 
+
 const Block = class {
   constructor(x, y) {
     this.x = x; 
@@ -15,10 +17,14 @@ const Block = class {
   update() {
     this.color = r.BLUE; 
     if (v.check_collision2d(this, v.object_get("player"))) this.color = r.DARKGREEN; 
+    if (v.check_collision2d(this, v.object_get('sensor')) && r.IsKeyDown(r.KEY_SPACE)) {
+      score += 1; 
+    }
   }    
   
   draw() {
     r.DrawRectangle(this.x, this.y, this.width, this.height, this.color); 
+    r.DrawText('' + score, this.x + 16, this.y - 32, 24, r.BLACK);
   }
 }
 
--- todo.md
+++ todo.md
@@ -2,18 +2,16 @@
 - Cant have multiple instances of one object.
 
 # Notes: 
-- might want to generate a unique instance id for each object created.
-	that way you have a way to check the type or check for a specific instance
-	of the object. very similar to how gms2 handles objs
 
 # TODO:
 - [ ] Raylib bindings :(
 - [ ] Key bindings
 - [ ] Delta Time
 - [ ] Vike
+	- [ ] Particles. 
 	- [ ] Depth Z ordering. 
-	- [ ] Collisions, 
-	- [ ] object handling 
+	- [x] Collisions, 
+	- [x] object handling 
 	- [ ] sprite drawing
 	- [ ] maps 
 	- [ ] raylib bindings for drawing and window, etc.
