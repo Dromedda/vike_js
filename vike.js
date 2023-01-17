caeb99275aa290017fe1155092127af9b822734a
Erik 'Dromedda' Gustafsson <erikgustafsson2001@gmail.com>
2023-01-17 19:36:54 +0100

    Added Pkg and added sprite drawing and loading

--- vike.js
+++ vike.js
@@ -25,6 +25,16 @@ Vike.log = function(str) {
 	console.log("VIKE::LOG::" + str); 
 }
 
+Vike.load_sprite = function(path = null) {
+	this.log("Tried loading a sprite, which isnt implemented yet..."); 
+	let ret = r.LoadTexture(path); 
+	return ret;
+}
+
+Vike.draw_sprite = function(spr, posx, posy, col) {
+	r.DrawTexture(spr, posx, posy, col); 
+}
+
 // TODOO: Might just wanna make the camera var local to vike, making it easier to use,
 // however will limit the amount of cameras to 1.
 Vike.camera_init = function(offset = vec2(0, 0), target = vec2(0, 0), rotation = 0, scale = 1) {
