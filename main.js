01d9e3576b662a1299cf594a82620671b62102cd
Erik 'Dromedda' Gustafsson <erikgustafsson2001@gmail.com>
2023-01-20 18:59:36 +0100

    removed everything related to the master example

--- main.js
+++ main.js
@@ -3,9 +3,7 @@ const r = require('raylib');
 // import the scenes and scene_handler
 const scene = require('./comps/scene_handler.js');
 const v = require('./vike.js'); 
-const main_scene = require('./src/scene_main.js'); 
-const intro_scene = require('./src/scene_intro.js'); 
-const menu_scene = require('./src/scene_menu.js'); 
+const scene_birf = require('./src/scene_birf');
 
 const window = {
 	height: 800, 
@@ -17,10 +15,8 @@ const window = {
 function main() {
 	v.init_window(window); 
 
-	scene.create("menu", menu_scene); 
-	scene.create("intro", intro_scene); 
-	scene.create("main", main_scene); 
-	scene.goto("intro"); 
+	scene.create('birf', scene_birf); 
+	scene.goto('birf');
 
 	while(!r.WindowShouldClose()) {
   	scene.update();
