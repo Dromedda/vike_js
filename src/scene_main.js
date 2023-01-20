d387557911a4f178295eb15b7bdf4e71d34f04d0
Erik 'Dromedda' Gustafsson <erikgustafsson2001@gmail.com>
2023-01-20 18:52:44 +0100

    added more sensors at the start of the game, added the assets for the animations

--- src/scene_main.js
+++ src/scene_main.js
@@ -7,6 +7,8 @@ const Block = require('./block.js')
 
 const SceneMain = {}; 
 
+let sensors =  []; 
+
 let player, sensor, block; 
 let camera_pos = v.vec2(0, 0); 
 let camera_offset = v.vec2(0, 0); 
@@ -15,14 +17,22 @@ const camera = v.camera_init(camera_offset, camera_pos, 0, 1);
 SceneMain.init = function() {
 	player = v.add_obj(new Player(100, 100), 'player'); 
 	sensor = v.add_obj(new Sensor(200, 200, 32, 32), 'sensor');
+	sensors.push(sensor);
 	block = v.add_obj(new Block(500, 200), 'block'); 
+
+	for (let i = 0; i < 20; i++) {
+		sensors.push(v.add_obj(new Sensor(Math.random() * r.GetScreenWidth(), Math.random() * r.GetScreenHeight(), 32, 32), 'sensor'+i)); 
+	}
 }
 
 SceneMain.update = function() {
 	v.camera_goto(camera, v.vec2(player.x, player.y), 16); 
 	player.update();
 	block.update();
-	sensor.update();
+	//sensor.update();
+	for (let i = 0; i < sensors.length; i++) {
+		sensors[i].update(); 
+	}
 }
 
 SceneMain.draw = function() {
@@ -32,7 +42,10 @@ SceneMain.draw = function() {
 	
 	block.draw();
 	player.draw();
-	sensor.draw();
+	//sensor.draw();
+	for (let i = 0; i < sensors.length; i++) {
+		sensors[i].draw(); 
+	}
 	v.camera_unset(); 
  	r.DrawFPS(4, 4);
 	v.log_frame_time();
