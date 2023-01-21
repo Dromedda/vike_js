d303a78a224c665bc3e1c3fa3f7953422736465f
Erik 'Dromedda' Gustafsson <erikgustafsson2001@gmail.com>
2023-01-21 13:32:55 +0100

    Added example and dev scene

---
+++ dev.js
@@ -0,0 +1,27 @@
+const r = require('raylib');
+const v = require('./vike.js');
+const scene = require('./comps/scene_handler.js');	
+const scene_dev = require('./src/scene_dev.js');
+
+const window = {
+	height: 900,
+	width: 1600,
+	title: "VIKE_JS - DEV",
+	targetFPS: 60
+};
+
+function main() {
+	v.init_window(window);
+	scene.create('dev', scene_dev);
+	scene.goto('dev');
+
+	while(!r.WindowShouldClose()) {
+		scene.update();
+		r.BeginDrawing();
+			scene.draw();
+		r.EndDrawing();
+	}	
+	r.CloseWindow();
+}
+
+main();
